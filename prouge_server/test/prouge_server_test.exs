163a6b3d524eaae80d5e1d3cfb338444c038f8bf
Adrian Salamon <adr.salamon@gmail.com>
2022-03-04 19:17:46 +0100

    Made movement work
    Co-authored-by: klarasan <klarasan@users.noreply.github.com>

--- prouge_server/test/prouge_server_test.exs
+++ prouge_server/test/prouge_server_test.exs
@@ -1,8 +1,35 @@
 defmodule ProugeServerTest do
   use ExUnit.Case
   doctest ProugeServer
+  alias ProugeServer.Game, as: Game
+  alias ProugeServer.Game.Player, as: Player
+  alias ProugeServer.Game.GameState, as: GameState
 
-  test "greets the world" do
-    assert ProugeServer.hello() == :world
+
+  test "add players work" do
+    Game.remove_all_players()
+    Game.add_player(self())
+
+    expected = %GameState{players: [%Player{pid: self()}]}
+
+    state = :sys.get_state(ProugeServer.Game)
+    assert state == expected
+  end
+
+  test "move players work" do
+    Game.remove_all_players()
+    pid1 = :c.pid(0,255,0)
+    pid2 = :c.pid(0,254,0)
+
+    Game.add_player(pid1)
+    Game.add_player(pid2)
+
+    Game.move_player(pid1, :right)
+    Game.move_player(pid2, :down)
+
+    expected = %GameState{players: [%Player{pid: pid2, x: 0, y: 1}, %Player{pid: pid1, x: 1, y: 0}]}
+
+    state = :sys.get_state(ProugeServer.Game)
+    assert state == expected
   end
 end
